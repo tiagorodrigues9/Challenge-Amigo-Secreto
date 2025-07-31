ad26973eb40c325aff36fcab559f8f51b66d46c9
Tiago Rodrigues <tr364634@gmail.com>
2025-07-31 17:20:42 -0300

    feat: Inclusão da apresentação da lista de nomes

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let listaAmigoSecreto = [];
 
-function exibirTexto(tag,texto){
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
-function adicionarAmigo(){
+function adicionarAmigo() {
     let amigoSecreto = document.querySelector('input').value;
 
-    if(amigoSecreto == ''){
-        alert('Por favor, insira um nome!')
-    }else{
-        listaAmigoSecreto.push(amigoSecreto)
-        console.log(listaAmigoSecreto)
-        limparCampo()
+    if (amigoSecreto == '') {
+        alert('Por favor, insira um nome!');
+    } else {
+        listaAmigoSecreto.push(amigoSecreto);
+        console.log(listaAmigoSecreto);
+        limparCampo();
+        listarAmigosSecretos();
     }
-
 }
 
-function limparCampo(){
-    amigoSecreto = document.querySelector('input')
-    amigoSecreto.value = ''
+function limparCampo() {
+    let amigoSecreto = document.querySelector('input');
+    amigoSecreto.value = '';
 }
 
-function listarAmigosSecretos(){
-    
+// Atualiza a lista na tela
+function listarAmigosSecretos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaAmigoSecreto.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = listaAmigoSecreto[i];
+        lista.appendChild(li);
+    }
 }
 
-
-function sortearAmigo(){
+function sortearAmigo() {
 
 }
